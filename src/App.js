2880226963a29c5c1ff864fb318b03485e57c2a4
wojeee <rosameera@borneodigital.co>
2023-11-22 11:49:10 +0800

    small change in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme'; // make sure the path is correct
-import Dashboard from './components/Dashboard';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import Dashboard from "./components/Dashboard"; // Assuming this is the correct path
+import "./App.css";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Dashboard /> {/* Only render the Dashboard component */}
     </ThemeProvider>
   );
 }
