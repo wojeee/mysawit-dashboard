f932cb0fd985d71c691e334b887b38a0f6843dea
wojeee <rosameera@borneodigital.co>
2023-11-28 11:16:39 +0800

    Create SenaraiAset page

---
+++ src/pages/SenaraiAset.js
@@ -0,0 +1,14 @@
+// In your pages/SenaraiAset.js file
+
+import React from "react";
+
+const SenaraiAset = () => {
+  return (
+    <div>
+      <h1>Senarai Aset</h1>
+      {/* Rest of your component */}
+    </div>
+  );
+};
+
+export default SenaraiAset;
